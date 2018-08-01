61622d6faed7523eb877166a2a89ea7e8cffbed9
unknown <lilin873669191@qq.com>
2018-08-01 10:09:44 +0800

    改正错误

--- jspromise.js
+++ jspromise.js
@@ -1,76 +1,57 @@
 var Mypromise = function (fn) {
-
     this.resloveevent = [];
     this.rejectevent = [];
     var state = 0;
-    var i = 1;
-    var event = "";
-    var events = "";
-    var othervalue = "";
+    var than = this;
+    var returnvalue = null;
+    var rejectvalue = null;
     this.then = function (fn) {
-        if(state == 1){
-        this.resloveevent.message.push(fn);
-        this.send("message", event, i);
-        i++;
+        if(state == 0){
+            this.resloveevent.push(fn);
+        }else if(state == 1){   
+            returnvalue = fn(returnvalue);
         }
         return this;
     }
     this.catch = function (fn) {
-        if(state == 2){
-        this.rejectevent.reject.push(fn);
-        i = 1;
-        this.send("reject", events, i);
+        if(state == 0){
+            this.rejectevent.push(fn);
+        }else if(state == 2){
+            fn(rejectvalue)
         }
+        return this;
+
     }
     var reslove = function (value) {
         state = 1;
-        event = value;
+        // console.log(than.resloveevent);
+        if(than.resloveevent.length == 0){
+            returnvalue = value;
+        }
+        for(var s = 0;s < than.resloveevent.length;s++){
+            if(s == 0){
+                returnvalue = than.resloveevent[s](value);
+            }else{
+                returnvalue = than.resloveevent[s](returnvalue);
+            }
+        }
     };
     var reject = function (value) {
         state = 2;
-        events = value;
+        if(than.rejectevent.length == 0){
+            rejectvalue = value;
+        }else{
+            than.rejectevent[0](value);
+        }
     }
     fn(reslove, reject);
-    this.subscribe("message", reslove);
-    this.subscribes("reject", reject);
-}
-Mypromise.prototype = {
-    //订阅
-    subscribe(type, fn) {
-        //查阅是否已经有这个type
-        if (!this.resloveevent[type]) {
-            this.resloveevent[type] = [];
-        }
-        this.resloveevent[type].push(fn);
-    },
-    subscribes(type, fn) {
-        //查阅是否已经有这个type
-        if (!this.rejectevent[type]) {
-            this.rejectevent[type] = [];
-        }
-        this.rejectevent[type].push(fn);
-    },
-    //发送订阅
-    send(type, event, i) {
-        if (type == "reject") {
-            // console.log(this.rejectevent.reject);
-            this.rejectevent.reject[i](event);
-        } else {
-            if (i == 1) {
-                this.othervalue = this.resloveevent.message[i](event);
-            } else {
-                this.othervalue = this.resloveevent.message[i](this.othervalue);
-            }
-        }
-    }
 }
 
 var test = function (time) {
     var one = new Mypromise(function (reslove, reject) {
-        console.log(time);
-        // console.log(reslove);
-        // setTimeout(reslove, time, "456");
-        reslove("456")
+        setTimeout(reslove, time, "457");
+        // reject("456");
+        // reslove("456");
     });
     return one;
 }
