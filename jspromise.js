71580f68ef308dbdc073ab7375309fa3e0655eda
unknown <lilin873669191@qq.com>
2018-08-01 13:24:29 +0800

    更改

--- jspromise.js
+++ jspromise.js
@@ -1,72 +1,94 @@
 var Mypromise = function (fn) {
-    this.resloveevent = [];
-    this.rejectevent = [];
+    var resolveevent = [];
+    var rejectevent = [];
     var state = 0;
-    var than = this;
+   
     var returnvalue = null;
     var rejectvalue = null;
     this.then = function (fn) {
+        return new Mypromise(function(resolve,reject){
+            dealresolve({
+                fn:fn,
+                resolve:resolve
+            });
+        })
+    }
+    function dealresolve(event){
         if(state == 0){
-            this.resloveevent.push(fn);
-        }else if(state == 1){   
-            returnvalue = fn(returnvalue);
+            resolveevent.push(event);
+            return;
         }
-        return this;
+        var ret = event.fn(returnvalue);
+        event.resolve(ret);
     }
     this.catch = function (fn) {
-        if(state == 0){
-            this.rejectevent.push(fn);
-        }else if(state == 2){
-            fn(rejectvalue)
-        }
-        return this;
-
+        return new Mypromise(function(resolve,reject){
+            dealreject({
+                fn:fn,
+                reject:reject
+            });
+        })
     }
-    var reslove = function (value) {
+    function dealreject(event){
+        if(state == 0){
+            rejectevent.push(event);
+            return;
+        }
+        var ret = event.fn(returnvalue);
+        event.reject(ret);
+    }
+    var resolve = function (value) {
         state = 1;
-        // console.log(than.resloveevent);
-        if(than.resloveevent.length == 0){
-            returnvalue = value;
-        }
-        for(var s = 0;s < than.resloveevent.length;s++){
-            if(s == 0){
-                returnvalue = than.resloveevent[s](value);
-            }else{
-                returnvalue = than.resloveevent[s](returnvalue);
+        returnvalue = value;
+        setTimeout(()=>{
+            for(var s = 0;s < resolveevent.length;s++){
+                dealresolve(resolveevent[s]);
             }
-        }
+        },1);
     };
     var reject = function (value) {
-        state = 2;
-        if(than.rejectevent.length == 0){
-            rejectvalue = value;
-        }else{
-            than.rejectevent[0](value);
-        }
+        state = 1;
+        returnvalue = value;
+        setTimeout(()=>{
+            for(var s = 0;s < rejectevent.length;s++){
+                dealreject(rejectevent[s]);
+            }
+        },1);
     }
-    fn(reslove, reject);
+    fn(resolve, reject);
 }
 
 var test = function (time) {
-    var one = new Mypromise(function (reslove, reject) {
-        setTimeout(reslove, time, "457");
+    var one = new Mypromise(function (resolve, reject) {
+        setTimeout(resolve, time, "457");
         // reject("456");
-        // reslove("456");
+        // resolve("456");
     });
     return one;
 }
-test(1000)
-    .then(function (s) {
-        console.log(s);
-        return "123234";
-    })
-    .then(function (s) {
-        console.log(s)
-        return "1010101010";
-    })
-    .then(function (s) {
-        console.log(s);
-    })
-    .catch(function (s) {
-        console.log("出现错误！" + s);
-    })
+// test(10000)
+//     .then(function (s) {
+//         console.log(s);
+//         return "123234";
+//     })
+//     .then(function (s) {
+//         console.log(s)
+//         return "1010101010";
+//     })
+//     .then(function (s) {
+//         console.log(s);
+//     })
+//     .catch(function (s) {
+//         console.log("出现错误！" + s);
+//     })
+var p = new test(1000);
+p.then((b)=>{
+    console.log(b);
+    return "789";
+})
+.then((b)=>{
+    console.log(b);
+})
+p.then((b)=>{
+    console.log(b);
+})
